8ec522b30badf0fafe97cfd5a629a1be7589c974
rjpm95 <killlo325@hotmail.com>
2022-01-23 05:29:47 +0000

    Location

--- lib/views/second_route.dart
+++ lib/views/second_route.dart
@@ -1,6 +1,12 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 class SecondRoute extends StatelessWidget {
+
+  Location location = Location();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,11 +15,30 @@ class SecondRoute extends StatelessWidget {
         backgroundColor: Colors.green,
       ),
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Text('Back!'),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              StreamBuilder(
+                  stream: location.onLocationChanged,
+                  builder: (context, snapshot){
+                    if(snapshot.connectionState != ConnectionState.waiting)
+                    {
+                      var data = snapshot.data as LocationData;
+
+                      return Text('Position:  ${data.latitude}/${data.longitude}');
+                    }
+                    else {
+                      return Center(child: CircularProgressIndicator());
+                    }
+                  }
+              ),
+              RaisedButton(
+                  onPressed: (){
+                Navigator.pop(context);
+              },
+              child: Text('Back'))
+            ]
         ),
       ),
     );
